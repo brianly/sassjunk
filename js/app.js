5c9919b0c965f208c0143f8950e468bbb777b347
Brian Lyttle <github@killaloo.org>
2014-01-04 22:57:11 -0500

    Initial changes to support Foundation 5.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+	topbar : {
+		custom_back_text: true,
+		back_text: '← Back',
+		mobile_show_parent_link: false
+	}
+});
